22a840678245e0efeda59460857ccd4a0b5c3a7e
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-12-23 16:59:52 +0100

    Add middleware for form validation

--- router/userRouter.ts
+++ router/userRouter.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import bcrypt from 'bcrypt'
 import { body, validationResult } from 'express-validator'
 
+import fieldValidation from './middleware/fieldValidation'
+
 const userRouter: express.Router = express.Router()
 const saltRound = 10
 
@@ -13,15 +15,7 @@ userRouter.get('/test',(req: express.Request, res: express.Response)=>{
   res.status(200).send(`<h3>Welcome to userRouter test path</h3>`)
 })
 
-userRouter.post('/registration', [
-  body('name').not().isEmpty().withMessage('name is required'),
-  body('email')
-    .not().isEmpty().withMessage('email is required')
-    .isEmail().withMessage('Is not an Email'),
-  body('password')
-    .not().isEmpty().withMessage('password is required')
-    .isLength({ min: 5 }).withMessage('password length cannot be less than 5')
-],
+userRouter.post('/registration', fieldValidation ,
   
 async(req: express.Request, res: express.Response)=>{
   let {name, email, password} = req.body
